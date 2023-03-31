a18767b4168bd8ac2c16edcac70c22eacc32f9a2
Javier Prieto <66081109+tatoveins@users.noreply.github.com>
2023-03-31 15:54:31 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+import analisisIP
+import rendimiento
+import temp
+
+def abrir_analisisIP():
+    analisisIP.run()
+
+def abrir_rendimiento():
+    rendimiento.run()
+
+def abrir_temp():
+    temp.run()
